91b073a69a8241fa03c1b939aa2e7de1e036a400
Kumar Harsh <kharsh39@gmail.com>
2023-09-28 23:04:43 +0530

    Update starter code

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express")
 
 const app = express()
+const port = 3000
 
 app.get("/health", (req, res) => res.json({status: "UP"}))
 
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`Server running at port: ${port}`);
+});
